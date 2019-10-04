31d768d7b324031916ab17d3d9a5178e561229a4
Duarte Meneses <duarte.meneses@sonarsource.com>
2019-10-04 10:20:08 -0500

    tre

--- travis.sh
+++ travis.sh
@@ -3,13 +3,13 @@
 #cat ../../../build.sh
 #ls -l ../../../
 #ls -l .
-pwd
+#pwd
 #echo "$0"
 #set -euo pipefail
 #set +x
 java -version
 echo "JAVA_HOME: $JAVA_HOME"
-export JAVA_HOME=`dirname $(dirname $(which java))`
-echo "NEW JAVA_HOME: $JAVA_HOME"
+#export JAVA_HOME=`dirname $(dirname $(which java))`
+#echo "NEW JAVA_HOME: $JAVA_HOME"
 echo "Parameters in environment: $SONARQUBE_SCANNER_PARAMS"
 sonar-scanner -v
